75e764a16ff6079515ae5ad1ab49ea6f782fb8c3
ekater1na <ekaterina_l@tut.by>
2019-04-30 00:37:22 +0300

    feat: add color bucket fuctional

--- palette/assets/scripts/index.js
+++ palette/assets/scripts/index.js
@@ -7,7 +7,7 @@ colorPickerEl.addEventListener("click", function(event){
 });
 
  window.state = {
-  currentTool: 'colorPicker',
+  currentTool: '',
 };
 
 colorPickerEl.addEventListener('click', (event) => {
@@ -32,3 +32,42 @@ document.addEventListener('click', (event) => {
   }
 });
 
+// Bucket
+
+const bucketEl = document.getElementById('bucket');
+console.log('bucket', bucket);
+
+bucketEl.addEventListener("click", function(event){
+  console.log('click', event);
+});
+
+ window.state = {
+  currentTool: '',
+};
+
+bucketEl.addEventListener('click', (event) => {
+  console.log('click', event);
+  window.state.currentTool = 'bucket';
+  bucketEl.classList.add('highlight-color-picker');
+});
+
+const headerEl1 = document.getElementById('header');
+
+headerEl1.addEventListener('click', () => {
+  window.state.currentTool = 'none';
+  bucketEl.classList.remove('highlight-color-picker');
+});
+
+document.addEventListener('click', (event) => {
+  const b = event && event.target || window.event.srcElement;
+  if (window.state.currentTool === 'bucket' && "canvas-item" == b.className) {
+    event.stopPropagation ? event.stopPropagation() : event.cancelBubble = !0;
+    event = window.getComputedStyle ? getComputedStyle(b, "") : b.currentStyle;
+    event = event.backgroundColor;
+    let c = b.getAttribute("color");
+    element=document.getElementById("chosen_color");
+    currentColor = getComputedStyle(element).backgroundColor;
+    b.style.backgroundColor = (c = event, currentColor);
+    b.setAttribute("color", c)
+  }
+});
