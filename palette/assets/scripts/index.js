17b58ca3caf605a8afee9599f984fe7e4115f994
ekater1na <ekaterina_l@tut.by>
2019-04-29 14:27:07 +0300

    feat: add http-server

---
+++ palette/assets/scripts/index.js
@@ -0,0 +1,15 @@
+const colorPickerEl = document.getElementById('color_picker');
+console.log('colorPickerEl', colorPickerEl);
+colorPickerEl.addEventListener("click", function(event){
+  console.log('click', event);
+});
+
+ window.state = {
+  currentTool: 'colorPicker',
+};
+
+document.addEventListener('click', (event) => {
+  // eslint-disable-next-line no-console
+  console.log('click', event);
+  console.log('color', event.target.style.backgroundColor);
+});
